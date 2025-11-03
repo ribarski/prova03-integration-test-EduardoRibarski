78001743423591d800e25ee072035b147fa78325
Eduardo Ribarski Scalco <eduardo.8723@alunosatc.edu.br>
2025-11-03 20:37:37 -0300

    teste06

--- src/support/pages/GremioPage.ts
+++ src/support/pages/GremioPage.ts
@@ -8,8 +8,6 @@ export class GremioPage extends BasePage {
     super(page); 
   }
 
-  // --- MÉTODOS DE AÇÃO ---
-
   async navegarParaHome() {
     await this.page.goto(config.gremio);
   }
@@ -18,8 +16,6 @@ export class GremioPage extends BasePage {
 
     await this.page.getByRole('link', { name: 'Notícias', exact: true }).click();
   }
-
-  // --- MÉTODOS DE VALIDAÇÃO (ASSERT) ---
 
   async validarTituloPrincipal() {
     await expect(this.page.getByRole('link', { name: 'Grêmio Foot-Ball Porto Alegrense', exact: true })).toBeVisible();
