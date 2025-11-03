fa973e188ac8501e4def3e14610f309dd9cacddf
Eduardo Ribarski Scalco <eduardo.8723@alunosatc.edu.br>
2025-11-03 20:44:44 -0300

    teste07

--- src/scenarios/ZeroStepExample.spec.ts
+++ src/scenarios/ZeroStepExample.spec.ts
@@ -2,8 +2,8 @@ import { test } from '@playwright/test';
 import { ai } from '@zerostep/playwright';
 
 test('zerostep example', async ({ page }) => {
-  await page.goto('https://demo.automationtesting.in/Register.html');
+  await page.goto('https://gremio.net/');
 
   const aiArgs = { page, test };
-  await ai('enter values in full name field', aiArgs);
+  await ai('Click in buy T-shirt', aiArgs);
 });
